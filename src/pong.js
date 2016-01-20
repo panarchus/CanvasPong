33c3d89dc35c5bc46636e6277732471344e7ea52
Mark Fay <markdfay@gmail.com>
2016-01-20 17:27:05 -0500

    Smoother gameplay
    Added requestAnimationFrame() for smoother rendering and got rid of
    nasty alerts that no one likes.

--- src/pong.js
+++ src/pong.js
@@ -7,6 +7,16 @@ var run = false;
 var up = false;
 var down = false;
 
+// single-player score = # of hits (exponential growth in speed)
+var score = 0;
+var gameover = false;
+
+function drawScore() {
+	ctx.font = "16px Arial";
+	ctx.fillStyle = "#0095DD";
+	ctx.fillText("Score: " + score, 100, 20);
+}
+
 // event listeners for key presses
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -27,18 +37,35 @@ function keyUpHandler(e) {
 var ball = {
 	x: canvas.width/2,
 	y: canvas.height-30,
-	radius: 10,
-	dx: 2,
-	dy: -2
+	radius: 20,
+	dx: 4,
+	dy: -4
 };
 
 // paddle details (x,y will correspond to top-left corner, like the canvas x,y)
 var paddle = {
-	h: 50,
+	h: 100,
 	w: 10,
 	x: 20,
-	y: 20
+	y: 20,
+	dy: 8
 };
+
+// reinitialize starting values
+function init() {
+	ball.x = canvas.width/2;
+	ball.y = canvas.height-30
+	ball.radius = 20;
+	ball.dx = 4;
+	ball.dy = -4;
+	
+	paddle.h = 100;
+	paddle.w = 10;
+	paddle.x = 20;
+	paddle.y = 20;
+	
+	gameover = false;
+}
 
 // paints the ball on the canvas
 function drawBall() {
@@ -57,13 +84,14 @@ function updateBall() {
 	if (ball.x + ball.dx > canvas.width-ball.radius) ball.dx = -ball.dx;
 	// collision with left wall results in GAME OVER
 	if (ball.x + ball.dx < ball.radius) {
-		alert("GAME OVER");
-		document.location.reload();
+		gameover = true;
+		return;
 	} else if (ball.x + ball.dx < paddle.x + paddle.w + ball.radius) {
 		// check for collision with paddle
 		if (ball.y >= paddle.y && ball.y <= paddle.y + paddle.h) {
 			// hit paddle, speed up by 1.5x
-			ball.dx = (-ball.dx * 3) / 2;
+			ball.dx = (-ball.dx * 5) / 4;
+			++score;
 		}
 	}
 	
@@ -81,23 +109,38 @@ function drawPaddle() {
 }
 
 function updatePaddle() {
-	if (up && paddle.y > 0) paddle.y -= 5;
-	if (down && paddle.y + paddle.h < canvas.height) paddle.y += 5;
+	if (up && paddle.y > 0) paddle.y -= paddle.dy;
+	if (down && paddle.y + paddle.h < canvas.height) paddle.y += paddle.dy;
 }
 
 /*
  * MAIN
  */
+var reset = true;
 function draw() {
 	if (run) {
+		if (reset) {
+			init();
+			reset = false;
+		}
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		updatePaddle();
 		updateBall();
 		drawPaddle();
 		drawBall();
+		drawScore();
 	}
+	
+	if (gameover) {
+		ctx.font = "32px Arial";
+		ctx.fillStyle = "#0095DD";
+		ctx.fillText("GAME OVER", 150, 150);
+		reset = true;
+		run = false;
+	}
+	requestAnimationFrame(draw);
 }
-
+draw();
 // run  draw() every 10 ms
-setInterval(draw, 10);
+//setInterval(draw, 10);
 
